9072e2534381534346f50fe5dda2645c649a2084
egor_istomin <witto24@gmail.com>
2020-06-05 15:27:11 +0500

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass');
+
+gulp.task('sass_task', function (done) {
+gulp.src('app/*.scss')
+.pipe( sass({outputStyle: 'compressed'}) )
+.pipe(gulp.dest('dist/html/css'));
+done();
+});
+
+var prettyHtml = require('gulp-pretty-html');
+
+var pug = require('gulp-pug');
+gulp.task('pug', function() {
+  return gulp.src("app/pug/pages/*.pug")
+      .pipe(pug())
+      .pipe(prettyHtml())
+      .pipe(gulp.dest("dist/html"))
+});
